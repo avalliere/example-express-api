c3362e6893681abb3f016f14833f34db87095f1a
Andrea Valliere <amvalliere@gmail.com>
2021-06-30 23:16:05 -0700

    adds server and get and post endpoints

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const app = express();
+const PORT = 8080;
+
+// express does not parse json from request body by default. So in line 6, we are telling it to use json middleware so we can use json in our request bodies
+app.use(express.json());
+
+// this configures the server
+app.listen(
+  PORT,
+  () => console.log(`Server running on http://localhost:${PORT}`)
+)
+
+// Endpoint: GET http://localhost:8080/kitten
+// 1st argument is the route
+// 2nd argument is the callback function - we pass in request and response objects here
+app.get('/kitten', (req, res) => {
+  res.status(200).send({
+    'id': 1,
+    'kitten': '🐈‍⬛',
+    'name': 'Magpie'
+  })
+});
+
+// Endpoint: POST http://localhost:8080/kitten/1
+// ':id' is dynamic - this route will handle any id we pass in
+app.post('/kitten/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    res.status(400).send({ message: 'Kitten needs a name!'})
+  }
+
+  res.status(201).send({
+    message: `Kitten ${id} created with name: ${name}`
+  });
+})
